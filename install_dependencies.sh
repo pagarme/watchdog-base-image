c3a55eb338b07cab85480c66ce06612be507cf31
Felipe Yukio <fe.yukio@gmail.com>
2019-04-16 18:08:23 -0300

    Add changes to manage secrets
    Add changes to manage secrets variables using circleCI and lock
    dependencies version.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-# Update the repositories
-apk update
+set -ex
 
 # Install dependencies
-apk add --no-cache --virtual build-dependencies gcc python3-dev musl-dev
-apk add postgresql-dev make
+apk add --no-cache --virtual build-dependencies \
+    gcc \
+    python3-dev \
+    musl-dev \
+    make
+# Install and keep psycopg2 dependency
+apk add postgresql-dev
 
 # Install Python requirements
+pip3 install --upgrade pip
 pip3 install --no-cache-dir -r requirements.txt
 
 # Remove the temporary dependencies
